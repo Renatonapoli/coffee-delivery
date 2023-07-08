da6ddf36cb55e2a0067c68a5ba25f2443fe8ed7e
Renato Napoli <renato.napoli@nagro.app>
2023-07-08 12:14:20 -0300

    Fix: Ajustes de layout / container global

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,49 +1,47 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const ContainerNavbar = styled.div`
- display: flex;
- justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
- align-items: center;
+  margin: 2rem 0;
 
- img {
-  font-size: 2.5rem;
- }
-`
+  img {
+    font-size: 2.5rem;
+  }
+`;
 
 export const ContainerLocationCart = styled.div`
-display: flex;
-align-items: center;
-
-
-`
+  display: flex;
+  align-items: center;
+`;
 
 export const Map = styled.span`
-display: flex;
-align-items: center;
-margin-right: 0.8rem;
+  display: flex;
+  align-items: center;
+  margin-right: 0.8rem;
 
-width: 8.938rem;
-height: 2.375rem;
-border-radius: 6px;
-justify-content: center;
+  width: 8.938rem;
+  height: 2.375rem;
+  border-radius: 6px;
+  justify-content: center;
 
-background: ${(props) => props.theme['purple-light']};
+  background: ${(props) => props.theme["purple-light"]};
 
-font-size: 0.75rem;
-
-`
+  font-size: 0.75rem;
+`;
 
 export const Cart = styled.button`
-width: 2.375rem;
-height: 2.375rem;
-border-radius: 6px;
-border: none;
-cursor: pointer;
+  width: 2.375rem;
+  height: 2.375rem;
+  border-radius: 6px;
+  border: none;
+  cursor: pointer;
 
-background: ${(props) => props.theme['yellow-ligth']};
+  background: ${(props) => props.theme["yellow-ligth"]};
 
-display: flex;
-justify-content: center;
-align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,12 +1,13 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const NavbarContainer = styled.div`
   max-width: 90.25rem;
-  
-  padding: 2.5rem;
+  height: calc(100vh - 10rem);
+  margin: auto;
+  padding: 0 9rem;
+
+  border-radius: 8px;
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  
-`
+`;
