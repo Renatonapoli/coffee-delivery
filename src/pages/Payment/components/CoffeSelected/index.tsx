6f64f63a4bb10140615e649ced21bf27916122bd
Renato Napoli <renato.napoli@nagro.app>
2023-07-20 16:59:56 -0300

    Feat: Layout do component dos cafés selecionados

--- src/pages/Payment/components/CoffeSelected/index.tsx
+++ src/pages/Payment/components/CoffeSelected/index.tsx
@@ -1,28 +1,45 @@
+import { Minus, Plus, Trash } from "@phosphor-icons/react";
 import {
   ButtonMin,
   ButtonPlus,
   ButtonRemove,
   ContainerButtons,
   ContainerCoffes,
+  CountQuantityCoffe,
+  Division,
   Header,
+  TitleCoffe,
+  ValueCoffe,
 } from "./styles";
 
 export function Coffe() {
   return (
-    <ContainerCoffes>
-      <img src="../../src/assets/illustrations/capuccino.svg" alt="" />
-      <Header>
-        <ContainerButtons>
-          <h1>Expresso Tradicional</h1>
-          <div>
-            <ButtonPlus>+</ButtonPlus>
-            <span>1</span>
-            <ButtonMin>-</ButtonMin>
-          </div>
-          <ButtonRemove>Remover</ButtonRemove>
-        </ContainerButtons>
-      </Header>
-      <p>R$ 9,90</p>
-    </ContainerCoffes>
+    <>
+      <ContainerCoffes>
+        <div>
+          <img src="../../src/assets/illustrations/capuccino.svg" alt="" />
+        </div>
+        <Header>
+          <TitleCoffe>Expresso Tradicional</TitleCoffe>
+          <ContainerButtons>
+            <div>
+              <ButtonPlus>
+                <Plus size={16} color="#8047f8" />
+              </ButtonPlus>
+              <CountQuantityCoffe>1</CountQuantityCoffe>
+              <ButtonMin>
+                <Minus size={16} color="#8047f8" />
+              </ButtonMin>
+            </div>
+            <ButtonRemove>
+              <Trash size={16} color="#8047f8" />
+              Remover
+            </ButtonRemove>
+          </ContainerButtons>
+        </Header>
+        <ValueCoffe>R$ 9,90</ValueCoffe>
+      </ContainerCoffes>
+      <Division />
+    </>
   );
 }
--- src/pages/Payment/components/CoffeSelected/styles.ts
+++ src/pages/Payment/components/CoffeSelected/styles.ts
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const ContainerCoffes = styled.div`
   display: flex;
   padding: 0.5rem 0.25rem;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: flex-start;
   align-self: stretch;
   gap: 20px;
@@ -13,31 +13,111 @@ export const ContainerCoffes = styled.div`
   img {
     width: 4rem;
     height: 4rem;
+    flex-shrink: 0;
   }
 `;
 
 export const Header = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+export const TitleCoffe = styled.h2`
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%;
 `;
 
 export const ContainerButtons = styled.div`
   display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  h1 {
-    font-family: Roboto;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 130%;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 0.5rem;
+
+  div {
+    display: flex;
+    height: 1rem;
+    padding: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+
+    border-radius: 6px;
+    background: ${(props) => props.theme["gray-400"]};
   }
 `;
 
-export const ButtonPlus = styled.button``;
+export const ButtonPlus = styled.button`
+  display: flex;
+  align-items: center;
+  width: 0.875rem;
+  height: 0.875rem;
 
-export const ButtonMin = styled.button``;
+  border: none;
+  background: ${(props) => props.theme["gray-400"]};
 
-export const ButtonRemove = styled.button``;
+  cursor: pointer;
+`;
+
+export const CountQuantityCoffe = styled.span`
+  font-family: Roboto;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%;
+`;
+
+export const ButtonMin = styled.button`
+  display: flex;
+  align-items: center;
+  width: 0.875rem;
+  height: 0.875rem;
+
+  border: none;
+  background: ${(props) => props.theme["gray-400"]};
+
+  cursor: pointer;
+`;
+
+export const ButtonRemove = styled.button`
+  display: flex;
+  height: 2rem;
+  padding: 0 0.5rem;
+  margin-left: 0.5rem;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+
+  font-family: Roboto;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%;
+  text-transform: uppercase;
+
+  border-radius: 6px;
+  border: none;
+  background: ${(props) => props.theme["gray-400"]};
+
+  cursor: pointer;
+`;
+
+export const ValueCoffe = styled.p`
+  margin-left: auto;
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 130%;
+`;
+
+export const Division = styled.hr`
+  align-self: stretch;
+  stroke-width: 1px;
+
+  stroke: ${(props) => props.theme["gray-400"]};
+`;
